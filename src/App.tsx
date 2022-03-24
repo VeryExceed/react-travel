1716d82a003d486936cab1e6d911a1db3035e9b9
luomiao <1084459061@qq.com>
2022-03-24 17:58:50 +0800

    feat: header & Menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import styles from "./App.module.css";
+import React from 'react'
+import styles from './App.module.css'
+import { Header, Footer, Carousel, SideMenu } from './components'
+import { Row, Col } from 'antd'
 
 function App() {
   return (
     <div className={styles.App}>
-      <header className={styles['App-header']}>
-        <img src={logo} className={styles['App-logo']} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className={styles['App-link']}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      {/* 页面内容 content */}
+      <div className={styles['page-content']}>
+        <Row style={{ marginTop: 20 }}>
+          <Col span={6}>
+            <SideMenu />
+          </Col>
+          <Col span={18}>
+            <Carousel />
+          </Col>
+        </Row>
+      </div>
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
