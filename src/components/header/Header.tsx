fc98c46cd5b87585060bd921c38276a3e4a8e25b
luomiao <1084459061@qq.com>
2022-03-26 22:30:28 +0800

    feat: redux

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,8 +3,23 @@ import styles from './Header.module.css'
 import logo from '../../assets/logo.svg'
 import { Layout, Typography, Input, Menu, Button, Dropdown } from 'antd'
 import { GlobalOutlined } from '@ant-design/icons'
+import {
+  useHistory,
+  useLocation,
+  useParams,
+  useRouteMatch
+} from 'react-router-dom'
 
 export const Header: React.FC = () => {
+  const history = useHistory()
+  const location = useLocation()
+  const params = useParams()
+  const match = useRouteMatch()
+  console.log('history ', history)
+  console.log('location ', location)
+  console.log('params ', params)
+  console.log('match ', match)
+
   return (
     <div className={styles['app-header']}>
       {/* top-header */}
@@ -16,7 +31,7 @@ export const Header: React.FC = () => {
             overlay={
               <Menu>
                 <Menu.Item>中文</Menu.Item>
-                <Menu.Item>英语</Menu.Item>
+                <Menu.Item>English</Menu.Item>
               </Menu>
             }
             icon={<GlobalOutlined />}
@@ -24,20 +39,22 @@ export const Header: React.FC = () => {
             语言
           </Dropdown.Button>
           <Button.Group className={styles['button-group']}>
-            <Button>注册</Button>
-            <Button>登陆</Button>
+            <Button onClick={() => history.push('register')}>注册</Button>
+            <Button onClick={() => history.push('signIn')}>登陆</Button>
           </Button.Group>
         </div>
       </div>
       <Layout.Header className={styles['main-header']}>
-        <img src={logo} className={styles['App-logo']} alt="logo" />
-        <Typography.Title level={3} className={styles.title}>
-          慕课旅游网
-        </Typography.Title>
-        <Input.Search
-          placeholder={'请输入旅游目的地、主题、或关键字'}
-          className={styles['search-input']}
-        />
+        <span onClick={() => history.push('/')}>
+          <img src={logo} className={styles['App-logo']} alt="logo" />
+          <Typography.Title level={3} className={styles.title}>
+            慕课旅游网
+          </Typography.Title>
+          <Input.Search
+            placeholder={'请输入旅游目的地、主题、或关键字'}
+            className={styles['search-input']}
+          />
+        </span>
       </Layout.Header>
       <Menu mode={'horizontal'} className={styles['main-menu']}>
         <Menu.Item key={1}>旅游首页</Menu.Item>
